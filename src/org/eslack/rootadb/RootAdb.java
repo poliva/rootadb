03e20632d10ee1509c8da352044007c7ba07eaa8
Scott Alexander-Bown <dev@scottyab.com>
2013-06-06 15:54:47 +0100

    target api 17 to give nice styling on ICS devices, new background pattern, swap nasty toggle button for check box, added code to enabled/disable boot receiver to save it running all the time.

--- src/org/eslack/rootadb/RootAdb.java
+++ src/org/eslack/rootadb/RootAdb.java
@@ -1,29 +1,25 @@
 /*
 rootadb
 (c) 2013 Pau Oliva Fora <pof[at]eslack[dot]org>
-*/
+ */
 package org.eslack.rootadb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.*;
-import android.content.Context;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class RootAdb extends Activity {
-	
+
 	private TextView outputView;
 	private Handler handler = new Handler();
-	private ToggleButton toggle;
-	
-        private Context context;
-        private Utils mUtils;
+	private Utils mUtils;
+	private CheckBox check;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -34,37 +30,52 @@ public class RootAdb extends Activity {
 
 		mUtils = new Utils(getApplicationContext());
 
-		outputView = (TextView)findViewById(R.id.outputView);
-
-		toggle = (ToggleButton) findViewById(R.id.togglebutton);
-
-		toggle.setClickable(false);
-
-		toggle.setChecked(isAdbRoot());
-
-		toggle.setClickable(true);
-
+		initViews();
 	}
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-                menu.add(Menu.NONE, 0, 0, "Settings");
-                return super.onCreateOptionsMenu(menu);
-        }
+	@Override
+	protected void onStart() {
+		super.onStart();
+		updateUi(isAdbRoot());
+	}
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-                switch (item.getItemId()) {
-                        case 0:
-                                startActivity(new Intent(this, SettingsActivity.class));
-                                return true;
-                }
-                return false;
-        }
+	private void initViews() {
+		outputView = (TextView) findViewById(R.id.outputView);
+
+		check = (CheckBox) findViewById(R.id.checkBox1);
+
+		check.setClickable(false);
+
+		updateUi(isAdbRoot());
+
+		check.setClickable(true);
+	}
+
+	private void updateUi(boolean rootadbEnabled) {
+		check.setText(rootadbEnabled ? R.string.button_on : R.string.button_off);
+		check.setChecked(rootadbEnabled);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE, 0, 0, "Settings");
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case 0:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	public void onToggleClicked(View view) {
-		toggle.setClickable(false);
-		boolean on = ((ToggleButton) view).isChecked();
+		check.setClickable(false);
+
+		boolean on = ((CheckBox) view).isChecked();
 
 		if (on) {
 			output("\nrestarting adb daemon with root privileges\n");
@@ -78,17 +89,18 @@ public class RootAdb extends Activity {
 			public void run() {
 				mUtils.doTheMeat();
 				isAdbRoot();
-				toggle.setClickable(true);
+				check.setClickable(true);
 			}
 		});
 		thread.start();
 
-		}
+	}
 
 	private void output(final String str) {
 		Runnable proc = new Runnable() {
 			public void run() {
-				if (str!=null) outputView.append(str);
+				if (str != null)
+					outputView.append(str);
 			}
 		};
 		handler.post(proc);
