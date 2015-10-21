553e789133fb8da67e7fd40f12fb75f5ef5108e3
Joshua J. Drake <github.jdrake@qoop.org>
2015-10-20 20:14:51 -0500

    Remove old binary linked fallback
    Since adding a backwards compatible implementation in d7a2bbc, we no longer
    need to include binaries and link against them. Further, these binaries are no
    longer compatible with newer versions of Android.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,5 +5,13 @@ LOCAL_SRC_FILES:= \
 	setpropex.c system_properties.c system_properties_compat.c
 LOCAL_MODULE := setpropex
 LOCAL_CFLAGS += -std=c99 -I jni/inc
-LOCAL_LDLIBS    := -llog libcutils.so libc.so
+LOCAL_LDLIBS := -llog
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= \
+	setpropex.c system_properties.c system_properties_compat.c
+LOCAL_MODULE := setpropex-pie
+LOCAL_CFLAGS += -std=c99 -I jni/inc
+LOCAL_LDLIBS := -llog -pie -fPIE
 include $(BUILD_EXECUTABLE)
